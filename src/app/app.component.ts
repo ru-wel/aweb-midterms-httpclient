d560abf0e8f711d29ba72844222184d815e18261
welru <reuelchristian.sundiam04@gmail.com>
2025-02-06 20:18:22 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpclientService } from './httpclient.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'http-client-prg';
+  httpcomments: any;
+  constructor(private httpclientList: HttpclientService){
+    this.httpclientList.getUsersRemotely().subscribe((data: any) => {
+      this.httpcomments = data.slice(0, 5);
+    });
+  }
 }
